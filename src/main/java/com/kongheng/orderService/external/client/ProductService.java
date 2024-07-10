ad894e2a777fbd0f35e02c02f489942924b8523c
kongheng-sbilh <kongheng.long@sbilhbank.com.kh>
2024-07-10 15:52:31 +0700

    feat: Calling ReduceQuantity API from placeOrder Servie using Feign Client

---
+++ src/main/java/com/kongheng/orderService/external/client/ProductService.java
@@ -0,0 +1,15 @@
+package com.kongheng.orderService.external.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "product-service/product")
+public interface ProductService {
+
+    @PutMapping("/reduceQuantity/{id}")
+    ResponseEntity<Void> reduceQuantity(@PathVariable("id") long productId, @RequestParam long quantity);
+
+}
--- src/main/java/com/kongheng/orderService/service/OrderServiceImpl.java
+++ src/main/java/com/kongheng/orderService/service/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package com.kongheng.orderService.service;
 
 import com.kongheng.orderService.entity.Order;
+import com.kongheng.orderService.external.client.ProductService;
 import com.kongheng.orderService.model.OrderRequest;
 import com.kongheng.orderService.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +15,14 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private ProductService productService;
+
     @Override
     public long placeOrder(OrderRequest request) {
+
+        productService.reduceQuantity(request.getProductId(), request.getQuantity());
+
         Order order = Order.builder()
             .amount(request.getTotalAmount())
             .orderStatus("CREATED")
